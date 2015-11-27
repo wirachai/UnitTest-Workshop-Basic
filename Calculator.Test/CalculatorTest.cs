7388f02886268f2b90e4526e12166cfa3a308afc
wirachai <wirachai.me@gmail.com>
2015-11-27 13:19:48 +0700

    Add more tests

--- Calculator.Test/CalculatorTest.cs
+++ Calculator.Test/CalculatorTest.cs
@@ -5,12 +5,49 @@ namespace Calculator.Test
     [TestFixture]
     internal class CalculatorTest
     {
-        [Test]
-        public void Add_ShouldReturn5_WhenInput5()
+        private Calculator cal;
+
+        [SetUp]
+        public void Setup()
         {
-            Calculator cal = new Calculator();
+            cal = new Calculator();
+        }
+
+        [Test]
+        public void Add_ShouldReturnPositive_WhenInputPositive()
+        {
             cal.Add(5);
             Assert.AreEqual(5, cal.Current);
+        }
+
+        [Test]
+        public void Add_ShouldReturnMinus_WhenInputMinus()
+        {
+            cal.Add(-5);
+            Assert.AreEqual(-5, cal.Current);
+        }
+
+        [Test]
+        public void Add_ShouldReturnDecimal_WhenInputDecimal()
+        {
+            cal.Add((decimal)123.4567);
+            Assert.AreEqual(123.4567, cal.Current);
+        }
+
+        [Test]
+        public void Add_ShouldBeAccumulated_WhenCurrentHasValue()
+        {
+            cal.Current = 10;
+            cal.Add(5);
+            Assert.AreEqual(15, cal.Current);
+        }
+
+        [Test]
+        public void Add_ShouldWork_WhenInputLargeNumber()
+        {
+            cal.Current = 10;
+            cal.Add((decimal)987654321.12);
+            Assert.AreEqual(987654331.12, cal.Current);
         }
     }
 }
