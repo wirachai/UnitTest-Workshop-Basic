e99d53eb7d9e03e4baaac24ee4b67995620afe39
wirachai <wirachai.me@gmail.com>
2015-11-27 12:02:51 +0700

    Create first test

---
+++ Calculator.Test/CalculatorTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace Calculator.Test
+{
+    [TestFixture]
+    internal class CalculatorTest
+    {
+        [Test]
+        public void Add_ShouldReturn5_WhenInput5()
+        {
+            Calculator cal = new Calculator();
+            cal.Add(5);
+            Assert.AreEqual(5, cal.Current);
+        }
+    }
+}
---
+++ Calculator/Calculator.cs
@@ -0,0 +1,12 @@
+﻿namespace Calculator
+{
+    public class Calculator
+    {
+        public decimal Current { get; set; }
+
+        public void Add(decimal number)
+        {
+            Current += number;
+        }
+    }
+}
